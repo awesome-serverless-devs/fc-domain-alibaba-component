397facec1e6400dfea9598d11289e09465aadbb5
千风 <qianfeng.zqf@alibaba-inc.com>
2021-03-26 14:03:58 +0800

    feat: init project
    add deploy/remove command
    
    BREAKING CHANGE: init project

---
+++ src/lib/fc/fc-client.ts
@@ -0,0 +1,27 @@
+import { HLogger, ILogger } from '@serverless-devs/core';
+import { ICredentials } from '../profile';
+import FC from '@alicloud/fc2';
+
+export abstract class FcClient {
+  @HLogger('FC-DOMAIN') logger: ILogger;
+
+  readonly credentials: ICredentials;
+  readonly region: string;
+  readonly fcClient: any;
+
+  static defaultClientTimeout = 6000000;
+
+  constructor(region: string, credentials: ICredentials) {
+    this.region = region;
+    this.credentials = credentials;
+    this.fcClient = new FC(this.credentials.AccountID, {
+      accessKeyID: this.credentials.AccessKeyID,
+      accessKeySecret: this.credentials.AccessKeySecret,
+      securityToken: this.credentials.SecurityToken,
+      region: this.region,
+      timeout: FcClient.defaultClientTimeout,
+    });
+  }
+
+  abstract validateConfig(): void;
+}
