3460589dd4fb5e0ee59ada6d8af4867c6d131351
wss-git <wssgryx@163.com>
2021-06-16 11:13:08 +0800

    refactor: version 0.0.9

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,17 +1,9 @@
 import * as core from '@serverless-devs/core';
-import { FcCustomDomain } from './lib/fc/custom-domain';
 import { IInputs } from './interface';
 export default class FcBaseComponent {
     logger: core.ILogger;
-    report(componentName: string, command: string, accountID?: string, access?: string): Promise<void>;
-    handlerInputs(inputs: IInputs): Promise<{
-        appName: string;
-        projectName: string;
-        access: string;
-        fcCustomDomain: FcCustomDomain;
-        args: string;
-        curPath: string;
-    }>;
+    private report;
+    private handlerInputs;
     deploy(inputs: IInputs): Promise<void>;
     remove(inputs: IInputs): Promise<void>;
 }
